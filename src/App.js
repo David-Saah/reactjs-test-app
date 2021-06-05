a7ed5d7931f68724d79b7d77f1db8a36f9704cdc
David Saah <dasorange.hope@gmail.com>
2021-06-05 11:09:23 +0000

    Declaring and using props with class components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import './Styles/App.css';
 import './Styles/Bootstrap/bootstrap.min.css'
 // import Home from './Components/Home'
 // import Table from './Components/Table';
-// import { Component } from 'react';
+import { Component } from 'react';
 
-let App = (props) => {
-  console.log(props)
-	//outputs {subject: "Clarice"}
-	
-  const subject = props.subject
-    return (
-      <h1>Hello {subject}</h1>
-    );
+// let App = (props) => {
+//   console.log(props)
+// 	//outputs {subject: "Clarice"}
+
+//   const subject = props.subject
+//     return (
+//       <h1>Hello {subject}</h1>
+//     );
+//   }
+
+//Declaring and using props with class components
+class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <h1>Hello {this.props.subject}</h1>
+      </div>
+    )
   }
+}
 
 export default App;
