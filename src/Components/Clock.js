de1b3e3f82575f97884f0692d21dc74236d2293c
David Saah <dasorange.hope@gmail.com>
2021-06-08 11:28:34 +0000

    Added Clock component

---
+++ src/Components/Clock.js
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+// import ReactDOM from 'react-dom'
+
+//Ticking clock
+class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        //is called when the component is rendered to the DOM for the first time(mounting)
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+            ) 
+    }
+
+    componentWillUnmount() {
+        //is called when the DOM producing the component is removed(unmounting)
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Hello, World!</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
+
+// let Tick = (props) => {
+//     ReactDOM.render(
+//         <Clock date={new Date()} />,
+//         document.getElementById("root")
+//     )
+// }
+
+// setInterval(Tick, 1000)
+
+/* side notes */
+/* componentDidMount and componentWillUnmount are called lifecycle methods */
+
+
+export default Clock;
